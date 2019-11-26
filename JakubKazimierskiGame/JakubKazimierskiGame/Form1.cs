cf98f1a3207df991030f63ae0d4eacbde7f4016f
JakubKazimierski <j.m.kazimierski@gmail.com>
2019-11-26 16:50:21 +0100

    added @

--- JakubKazimierskiGame/JakubKazimierskiGame/Form1.cs
+++ JakubKazimierskiGame/JakubKazimierskiGame/Form1.cs
@@ -162,12 +162,12 @@ namespace JakubKazimierskiGame
             //methods to load pictures of enemies
             
            
-            Image enemi1 = Image.FromFile(@"./images\\E1.png");
-            Image enemi2 = Image.FromFile(@"./images\\E2.png");
-            Image enemi3 = Image.FromFile(@"./images\\E3.png");
+            Image enemi1 = Image.FromFile(@"images\\E1.png");
+            Image enemi2 = Image.FromFile(@"images\\E2.png");
+            Image enemi3 = Image.FromFile(@"images\\E3.png");
 
-            Image boss1 = Image.FromFile(@"./images\\Boss1.png");
-            Image boss2 = Image.FromFile(@"./images\\Boss2.png");
+            Image boss1 = Image.FromFile(@"images\\Boss1.png");
+            Image boss2 = Image.FromFile(@"images\\Boss2.png");
 
 
             //create enemies img
